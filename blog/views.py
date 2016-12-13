9f73bc1831367d3d8727414a65a7e15cc44b6afc
woodme0000 <luojingen@wangruichendeMacBook-Pro.local>
2016-12-13 23:33:42 +0800

    解决了detail页面问题

--- blog/views.py
+++ blog/views.py
@@ -8,10 +8,7 @@ def post_list(request):
     return render(request,'blog/post/list.html',{'posts':posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post,
-                                   status='published',
-                                   publish__year=year,
-                                   publish__month=month,
-                                   publish__day=day)
-    print (post.title)
+    #post =get_object_or_404(Post,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+    post = get_object_or_404(Post,slug=post,status='published',publish__year=year)
+    print('----DEBUG: 我是post_detail，我被调用了 post的标题是:%s',post)
     return render(request, 'blog/post/detail.html', {'post': post})
