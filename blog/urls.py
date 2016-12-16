65ee78051675c6648ce381fbef817b87e8c5a1dd
woodme0000 <luojingen@wangruichendeMacBook-Pro.local>
2016-12-16 23:43:48 +0800

    kkk,看完第一张的

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import post_list,post_detail
+from .views import post_list,post_detail,PostListView
 app_name = "blog"
 
 urlpatterns =[
-    url(r'^$',post_list,name='post_list'),
+    url(r'^$',PostListView.as_view(),name='post_list'),
+    #url(r'^$',post_list,name='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',post_detail,name='post_detail'),
 ]
